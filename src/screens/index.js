1f82697c24617a19cdc630b8af48a6d9e0ca0612
uu29 <uu5474@gmail.com>
2021-01-06 20:32:54 +0900

    Nested Navigator: 헤더 - 탭 네비게이션 구현 (Tab > Stack)

--- src/screens/index.js
+++ src/screens/index.js
@@ -11,56 +11,83 @@ import NewsScreen from './NewsScreen';
 import ProfileScreen from './ProfileScreen';
 
 const Tab = createBottomTabNavigator();
+const FavoritesStack = createStackNavigator();
+const SearchStack = createStackNavigator();
+const CommunityStack = createStackNavigator();
+const NewsStack = createStackNavigator();
+const ProfileStack = createStackNavigator();
 
-const TabNavigator = () => (
-  <Tab.Navigator
-    screenOptions={({route}) => ({
-      tabBarIcon: ({focused, color, size}) => {
-        let iconName;
-        // focused: bool, 클릭했는지 여부
-
-        switch (route.name) {
-          case 'Search':
-            iconName = 'magnify';
-            break;
-          case 'Community':
-            iconName = 'forum-outline';
-            break;
-          case 'News':
-            iconName = 'view-dashboard-outline';
-            break;
-          case 'Profile':
-            iconName = 'account-circle-outline';
-            break;
-          default:
-            iconName = 'star-outline';
-        }
-        return (
-          <MaterialCommunityIcons size={size} name={iconName} color={color} />
-        );
-      },
-    })}
-    tabBarOptions={{
-      activeTintColor: '#1B2228', // 활성화 되었을 때 색
-      inactiveTintColor: '#C7CDD3', // 비활성화 색
-      showLabel: false, // 텍스트 숨기기
-    }}>
-    <Tab.Screen name="Favorites" component={FavoritesScreen} />
-    <Tab.Screen name="Search" component={SearchScreen} />
-    <Tab.Screen name="Community" component={CommunityScreen} />
-    <Tab.Screen name="News" component={NewsScreen} />
-    <Tab.Screen name="Profile" component={ProfileScreen} />
-  </Tab.Navigator>
+const FavoritesStackNavigator = () => (
+  <FavoritesStack.Navigator>
+    <FavoritesStack.Screen name="Favorites" component={FavoritesScreen} />
+  </FavoritesStack.Navigator>
 );
-
-const Stack = createStackNavigator();
+const SearchStackNavigator = () => (
+  <SearchStack.Navigator>
+    <SearchStack.Screen name="Search" component={SearchScreen} />
+  </SearchStack.Navigator>
+);
+const CommunityStackNavigator = () => (
+  <CommunityStack.Navigator>
+    <CommunityStack.Screen name="Community" component={CommunityScreen} />
+  </CommunityStack.Navigator>
+);
+const NewsStackNavigator = () => (
+  <NewsStack.Navigator>
+    <NewsStack.Screen name="News" component={NewsScreen} />
+  </NewsStack.Navigator>
+);
+const ProfileStackNavigator = () => (
+  <ProfileStack.Navigator>
+    <ProfileStack.Screen name="Profile" component={ProfileScreen} />
+  </ProfileStack.Navigator>
+);
 
 function AppStack() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Stack" component={TabNavigator} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+            // focused: bool, 클릭했는지 여부
+
+            switch (route.name) {
+              case 'Search':
+                iconName = 'magnify';
+                break;
+              case 'Community':
+                iconName = 'forum-outline';
+                break;
+              case 'News':
+                iconName = 'view-dashboard-outline';
+                break;
+              case 'Profile':
+                iconName = 'account-circle-outline';
+                break;
+              default:
+                iconName = 'star-outline';
+            }
+            return (
+              <MaterialCommunityIcons
+                size={size}
+                name={iconName}
+                color={color}
+              />
+            );
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#1B2228', // 활성화 되었을 때 색
+          inactiveTintColor: '#C7CDD3', // 비활성화 색
+          showLabel: false, // 텍스트 숨기기
+        }}>
+        <Tab.Screen name="Favorites" component={FavoritesStackNavigator} />
+        <Tab.Screen name="Search" component={SearchStackNavigator} />
+        <Tab.Screen name="Community" component={CommunityStackNavigator} />
+        <Tab.Screen name="News" component={NewsStackNavigator} />
+        <Tab.Screen name="Profile" component={ProfileStackNavigator} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
