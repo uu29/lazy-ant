e4d5eff8f2451466b657357dabd40898a96d3ce9
uu29 <uu5474@gmail.com>
2021-01-09 13:40:38 +0900

    interceptor 에 custome headers 적용

--- src/utils/client.js
+++ src/utils/client.js
@@ -8,15 +8,22 @@ const YahooClient = axios.create({
     // 커스텀헤더를 넣으면 오류남. 커스텀헤더에는 ' 가 생김.
     Accept: 'application/json',
     useQueryString: 'true',
+    'x-rapidapi-key': '7cd6735678msh7d3a80d86d9f15ep1739d8jsn0b3dc896f11c',
+    'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
   },
 });
 
 // 요청 인티셉터 (요청 전에 가로채서 axios 설정을 적용함)
 YahooClient.interceptors.request.use(
   function (config) {
-    console.log(config);
-    // 요청 성공 직전 호출
-    // axios 설정값을 넣음 (사용자 정의 설정도 추가 가능)
+    const headers = {
+      x_rapidapi_key: '7cd6735678msh7d3a80d86d9f15ep1739d8jsn0b3dc896f11c',
+      x_rapidapi_host: 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+    };
+
+    const {x_rapidapi_key, x_rapidapi_host} = headers;
+    config.headers['x-rapidapi-key'] = x_rapidapi_key;
+    config.headers['x-rapidapi-host'] = x_rapidapi_host;
     return config;
   },
   function (error) {
--- src/utils/favorites/index.js
+++ src/utils/favorites/index.js
@@ -1,13 +1,8 @@
 import yahooClient from '../client';
 
 const _fetchHistoricalData = (params) => {
-  const headers = {
-    'x-rapidapi-key': '7cd6735678msh7d3a80d86d9f15ep1739d8jsn0b3dc896f11c',
-    'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
-  };
-
   return yahooClient
-    .get(`/stock/v3/get-historical-data`, {params: params, headers: headers})
+    .get(`/stock/v3/get-historical-data`, {params: params})
     .then((res) => {
       return res;
     })
