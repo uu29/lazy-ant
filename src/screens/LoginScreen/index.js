9d69a879fd38a483aea50d1de63ec042cde914c6
uu29 <uu5474@gmail.com>
2021-02-02 06:18:04 +0900

    SvgXml 이용하여 아이콘 import, 회원가입 퍼블리싱중

--- src/screens/LoginScreen/index.js
+++ src/screens/LoginScreen/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {SvgUri, SvgXml} from 'react-native-svg';
+// import Logo from '../../assets/images/logo-big.svg';
+import LogoBig from './LogoBig';
 
 function LoginScreen({onPress}) {
   return (
     <View style={styles.container}>
-      <View>
-        <Text>회원가입</Text>
+      <LogoBig />
+      <View style={styles.registerTextContainer}>
+        <Text style={styles.registerText}>회원가입</Text>
+      </View>
+      <View style={styles.btn}>
+        <Text style={styles.btnText}>이메일로 시작하기</Text>
       </View>
     </View>
   );
@@ -16,6 +23,26 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  registerTextContainer: {
+    marginVertical: 10,
+    marginBottom: 70,
+  },
+  registerText: {
+    marginTop: 10,
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  btn: {
+    borderColor: '#000',
+    borderWidth: 1,
+    borderRadius: 2,
+  },
+  btnText: {
+    fontSize: 20,
+    paddingVertical: 14,
+    width: 270,
+    textAlign: 'center',
   },
 });
 
